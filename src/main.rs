cbb133ca2bb7c7e04eec0076b8e516b372e8e8ad
Johan Flensmark <jokf@itu.dk>
2022-11-19 01:37:53 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::{
+    fs::{self},
+    process::{Command, Stdio},
+    thread,
+};
+use websocket::{ClientBuilder, OwnedMessage};
+
+fn main() {
+    let token = read_token();
+    Command::new("./osu_memory/gosumemory.exe")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    let client = ClientBuilder::new("ws://localhost:24050/ws")
+        .unwrap()
+        .connect_insecure()
+        .unwrap();
+
+    let (mut receiver, _) = client.split().unwrap();
+
+    thread::spawn(move || {
+        let mut last_updated = std::time::Instant::now();
+
+        for message in receiver.incoming_messages() {
+            if last_updated.elapsed().as_secs() > 5 {
+                last_updated = std::time::Instant::now();
+                if let Ok(msg) = message {
+                    match handle_update_message(msg, &token) {
+                        Ok(_) => (),
+                        Err(e) => println!("Error: {:?}", e),
+                    }
+                }
+            }
+        }
+    })
+    .join()
+    .unwrap();
+}
+
+fn handle_update_message(message: OwnedMessage, token: &str) -> Result<(), HandleError> {
+    match message {
+        OwnedMessage::Text(text) => {
+            let message: Value = serde_json::from_str(&text).unwrap();
+
+            let beatmap_info: &Value = message
+                .get("menu")
+                .and_then(|v| v.get("bm"))
+                .ok_or(HandleError::new("bm"))?;
+
+            let time: &Value = beatmap_info.get("time").ok_or(HandleError::new("time"))?;
+            let curr_time = get_i64(&time, "current")?;
+
+            let max_time = get_i64(&time, "mp3")?;
+
+            let meta_data = beatmap_info
+                .get("metadata")
+                .ok_or(HandleError::new("metadata"))?;
+
+            let currently_playing = NowPlaying {
+                token: token.to_string(),
+                title: get_string(&meta_data, "title")?,
+                artist: get_string(&meta_data, "artist")?,
+                current_time: curr_time,
+                full_time: max_time,
+                beatmap_id: get_i64(&beatmap_info, "set")?.to_string(),
+                difficulty: get_string(&meta_data, "difficulty")?,
+                timestamp: std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_millis(),
+            };
+            send_update_reqeust(currently_playing);
+
+            Ok(())
+        }
+        _ => Err(HandleError::new("Unknown message type")),
+    }
+}
+
+fn send_update_reqeust(np: NowPlaying) -> bool {
+    reqwest::blocking::Client::new()
+        .post("https://osu.bitknox.me/playing/update")
+        .json(&PostBody {
+            token: np.token.to_string(),
+            data: np,
+        })
+        .timeout(std::time::Duration::from_secs(10))
+        .send()
+        .is_ok()
+}
+
+fn get_i64(value: &Value, key: &str) -> Result<i64, HandleError> {
+    value
+        .get(key)
+        .and_then(|v| v.as_i64())
+        .ok_or(HandleError::new(key))
+}
+
+fn get_string(val: &Value, key: &str) -> Result<String, HandleError> {
+    Ok(val
+        .get(key)
+        .ok_or(HandleError::new(key))?
+        .as_str()
+        .ok_or(HandleError::new(key))?
+        .to_string())
+}
+
+fn read_token() -> String {
+    fs::read_to_string("token.txt").unwrap().to_string()
+}
+
+#[derive(Debug, Clone)]
+enum HandleError {
+    Token(String),
+}
+impl HandleError {
+    fn new(token: &str) -> HandleError {
+        HandleError::Token(token.to_string())
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct PostBody {
+    token: String,
+    data: NowPlaying,
+}
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NowPlaying {
+    pub token: String,
+    pub title: String,
+    #[serde(rename(serialize = "currentTime"))]
+    pub current_time: i64,
+    #[serde(rename(serialize = "fullTime"))]
+    pub full_time: i64,
+    #[serde(rename(serialize = "beatmapId"))]
+    pub beatmap_id: String,
+    pub artist: String,
+    pub difficulty: String,
+    pub timestamp: u128,
+}
